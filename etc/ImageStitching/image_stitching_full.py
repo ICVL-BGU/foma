0b01d2d94241f345fd1cbafe9c016586b8729467
AlexLazarovich <alexlazarovch@gmail.com>
2024-12-17 01:03:16 +0200

    Resizing added

--- etc/ImageStitching/image_stitching_full.py
+++ etc/ImageStitching/image_stitching_full.py
@@ -9,6 +9,7 @@ import pickle
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 import largestinteriorrectangle as lir
+import time
 
 # Constants for calibration flags
 CV_CALIB_USE_INTRINSIC_GUESS = 1
@@ -26,6 +27,7 @@ class ChessboardMatcher:
         self.pattern_size = pattern_size
         self.corners1 = self.detect_multiple_chessboards(img1)
         self.corners2 = self.detect_multiple_chessboards(img2)
+        logging.info(f"Found {len(self.corners1)} chessboards in image 1 and {len(self.corners2)} chessboards in image 2.")
         self.selected_corners1 = []
         self.selected_corners2 = []
         self.matched_pairs = set()
@@ -34,11 +36,15 @@ class ChessboardMatcher:
         self.current_pair = (0, 0)
         
     def detect_multiple_chessboards(self, img):
+        img = img.copy()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('Chessboard Detection', gray)
+        # cv2.waitKey(0)
         corners_list = []
         found = True
         while found:
             found, corners = cv2.findChessboardCorners(gray, self.pattern_size, None)
+            # logging.info(f"Found chessboard: {found}")
             if found:
                 cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), 
                                  (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
@@ -128,14 +134,13 @@ def find_chessboards(gray_im, pattern_size=(6, 9)):
     search_region = gray_im.copy()
 
     while True:
-        pattern_found, corners = cv2.findChessboardCorners(search_region, pattern_size)
+        pattern_found, corners = cv2.findChessboardCorners(search_region, pattern_size, flags = cv2.CALIB_CB_FAST_CHECK)
         if pattern_found:
             refined_corners = cv2.cornerSubPix(gray_im, corners, (11, 11), (-1, -1), 
-                                               criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+                                               criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)) 
             chessboards.append((objp, refined_corners))
-            # Mask out the detected chessboard area to find another one in the remaining image
             mask = np.zeros_like(search_region)
-            cv2.fillConvexPoly(mask, np.int32([refined_corners]), (255, 255, 255).tolist())
+            cv2.fillConvexPoly(mask, np.int32(refined_corners.reshape(-1, 2)), (255, 255, 255))
             search_region = cv2.bitwise_and(search_region, search_region, mask=cv2.bitwise_not(mask))
         else:
             break
@@ -157,7 +162,7 @@ def parse_ranges(camera_range_str, max_camera_num):
         camera_indices = list(range(1, max_camera_num + 1))
     return camera_indices
 
-def calibrate_cameras(base_dir, calib_range, fx, fy, cx, cy):
+def calibrate_cameras(base_dir, calib_range, fx, fy, cx, cy, resize_factor):
     cameras = os.path.join(base_dir, 'cameras')
     subdirectories = [d for d in os.listdir(cameras)]
     max_camera_num = max([int(d) for d in subdirectories if d.isdigit()], default=0)
@@ -180,7 +185,8 @@ def calibrate_cameras(base_dir, calib_range, fx, fy, cx, cy):
         for i, img_path in enumerate(glob.glob(camera_dir + '/*.jpg')):
             logging.info("Processing image %d/%d: %s", i+1, len(glob.glob(camera_dir + '/*.jpg')) ,img_path)
             im = cv2.imread(img_path)
-            gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            resized_im = cv2.resize(im, (0, 0), fx=resize_factor, fy=resize_factor)
+            gray_im = cv2.cvtColor(resized_im, cv2.COLOR_BGR2GRAY)
 
             chessboards = find_chessboards(gray_im)
             for j, (objp, corners) in enumerate(chessboards):
@@ -192,10 +198,10 @@ def calibrate_cameras(base_dir, calib_range, fx, fy, cx, cy):
 
         # Initialize matrix initial coefficient for intrinsic_guess is true
         matrix_init = np.zeros((3, 3), np.float32)
-        matrix_init[0][0] = fx  # f_x
-        matrix_init[0][2] = cx  # c_x
-        matrix_init[1][1] = fy  # f_y
-        matrix_init[1][2] = cy  # c_y
+        matrix_init[0][0] = fx * resize_factor  # f_x
+        matrix_init[0][2] = cx * resize_factor  # c_x
+        matrix_init[1][1] = fy * resize_factor  # f_y
+        matrix_init[1][2] = cy * resize_factor  # c_y
         matrix_init[2][2] = 1.0
         dist_init = np.zeros((1, 4), np.float32)
 
@@ -223,12 +229,14 @@ def calibrate_cameras(base_dir, calib_range, fx, fy, cx, cy):
     logging.info(f"All calibration parameters saved to {calib_params_path}.")
     return calib_params
 
-def match_images(base_dir, match_range, calib_params):
+def match_images(base_dir, match_range, calib_params, resize_factor):
     images_path = os.path.join(base_dir, 'final_images')
     max_image_num = len(os.listdir(images_path))
 
     images_indices = parse_ranges(match_range, max_image_num)
-    images = {i: cv2.imread(os.path.join(images_path, f"{i}.jpg")) for i in images_indices}
+    # images = {i: cv2.imread(os.path.join(images_path, f"{i}.jpg")) for i in images_indices}
+
+    images = {i: cv2.resize(cv2.imread(os.path.join(images_path, f"{i}.jpg")), (0, 0), fx=resize_factor, fy=resize_factor) for i in images_indices}
 
     matched_points_path = os.path.join(base_dir, "matched_points.pkl")
     if os.path.exists(matched_points_path):
@@ -412,115 +420,131 @@ def compute_homographies(base_dir, homographies_range, calib_params, matched_poi
 
 # Stitching the ROI of the images instead of the entire image, also dropping the base image
 
-# def feather_blending(images, masks, output_shape):
-#     blender = cv2.detail_FeatherBlender()
-#     blender.setSharpness(1.0 / 50.0)  # Adjust sharpness for better blending
-#     blender.prepare((0, 0, output_shape[1], output_shape[0]))
-#     for img, mask in zip(images, masks):
-#         blender.feed(cv2.UMat(img.astype(np.int16)), cv2.UMat(mask), (0, 0))
-#     result, result_mask = blender.blend(None, None)
-#     return result.get() if isinstance(result, cv2.UMat) else result
+def feather_blending(images, masks, output_shape):
+    blender = cv2.detail_FeatherBlender()
+    blender.setSharpness(1.0 / 50.0)  # Adjust sharpness for better blending
+    blender.prepare((0, 0, output_shape[1], output_shape[0]))
+    for img, mask in zip(images, masks):
+        blender.feed(cv2.UMat(img.astype(np.int16)), cv2.UMat(mask), (0, 0))
+    result, result_mask = blender.blend(None, None)
+    return result.get() if isinstance(result, cv2.UMat) else result
 
-# def find_and_save_largest_inscribed_rectangle(mask, image, filepath):
-#     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     largest_contour = max(contours, key=cv2.contourArea)
+def find_and_save_largest_inscribed_rectangle(mask, image, filepath):
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = max(contours, key=cv2.contourArea)
 
-#     # Fill the contour to ensure no internal black pixels
-#     cv2.drawContours(mask, [largest_contour], -1, 255, thickness=cv2.FILLED)
+    # Fill the contour to ensure no internal black pixels
+    cv2.drawContours(mask, [largest_contour], -1, 255, thickness=cv2.FILLED)
 
-#     mask_bool = mask.astype(bool)  # Convert mask to boolean array
-#     largest_rect = lir.lir(mask_bool, largest_contour[:, 0, :])
+    mask_bool = mask.astype(bool)  # Convert mask to boolean array
+    largest_rect = lir.lir(mask_bool, largest_contour[:, 0, :])
 
-#     with open(filepath, 'wb') as f:
-#         pickle.dump(largest_rect, f)
+    with open(filepath, 'wb') as f:
+        pickle.dump(largest_rect, f)
 
-#     x, y, w, h = largest_rect
-#     cropped_image = image[y:y+h, x:x+w]
-#     return cropped_image
+    x, y, w, h = largest_rect
+    cropped_image = image[y:y+h, x:x+w]
+    return cropped_image
 
-# def stitch_images(base_dir, calib_params, homographies, base_idx, num_bands=5, sigma=1):
-#     images_path = os.path.join(base_dir, 'final_images')
-#     images_files = [f for f in os.listdir(images_path) if f.lower().endswith(('.jpg', '.png', '.bmp', '.jpeg'))]
-#     images_indices = [int(os.path.splitext(f)[0]) for f in images_files]
-#     images = {i: cv2.imread(os.path.join(images_path, f"{i}.jpg")) for i in images_indices}
-
-#     # Step 1: Undistort Images
-#     for i in images_indices:
-#         K, D, mtx = calib_params[i]['K'], calib_params[i]['D'], calib_params[i]['mtx']
-#         images[i] = cv2.undistort(images[i], K, D, None, mtx)
-
-#     base_img = images[base_idx]
-#     h, w = base_img.shape[:2]
-
-#     corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype='float32').reshape(-1, 1, 2)
-#     all_corners = []
-#     all_corners.extend(corners)
-#     for i in images_indices:
-#         if i == base_idx:
-#             continue
-#         if (base_idx, i) in homographies:
-#             H = homographies[(base_idx, i)]
-#             warped_corners = cv2.perspectiveTransform(corners, H)
-#             all_corners.extend(warped_corners)
-
-#     all_corners = np.array(all_corners)
-#     [xmin, ymin] = np.int32(all_corners.min(axis=0).ravel())
-#     [xmax, ymax] = np.int32(all_corners.max(axis=0).ravel())
-#     t = [-xmin, -ymin]
-#     H_translate = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]], dtype=np.float32)
-
-#     output_width = xmax - xmin
-#     output_height = ymax - ymin
-
-#     warped_images = []
-#     masks = []
-#     for i in images_indices:
-#         if i == base_idx:
-#             continue
-#         H = H_translate @ homographies[(base_idx, i)]
-#         warped_image = cv2.warpPerspective(images[i], H, (output_width, output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
-
-#         # Create a mask based on the ROI
-#         roi = calib_params[i]['roi']
-#         x, y, w, h = roi
-#         mask = np.zeros((images[i].shape[0], images[i].shape[1]), dtype=np.uint8)
-#         mask[y:y+h, x:x+w] = 255
-
-#         # Warp the mask using the same homography
-#         warped_mask = cv2.warpPerspective(mask, H, (output_width, output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
-
-#         # Apply the mask to the warped image
-#         masked_warped_image = cv2.bitwise_and(warped_image, warped_image, mask=warped_mask)
-
-#         masks.append(warped_mask)
-#         warped_images.append(masked_warped_image)
-
-#     # Step 3: Blend images with feather blending
-#     result = feather_blending(warped_images, masks, (output_height, output_width))
-#     stitched_img = cv2.normalize(result, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-#     stitched_img = np.clip(stitched_img, 0, 255).astype(np.uint8)
-#     stitched_image_rgb = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2RGB)
-
-#     # Step 4: Find and crop the largest inscribed rectangle
-#     mask = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2GRAY)
-#     _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+def stitch_images(base_dir, calib_params, homographies, base_idx, resize_factor, num_bands=5, sigma=1):
+    if resize_factor != 1.0:
+        # logging.warning("Resizing images to %.2f", resize_factor)
+        S = np.array([[resize_factor, 0, 0],
+              [0, resize_factor, 0],
+              [0, 0, 1]])
+        for h in homographies:
+            # logging.info(f"Resizing homography ({h[0]}, {h[1]})")
+            # logging.info(f"Resizing homography ({homographies[h]})")
+            homographies[h] = S @ homographies[h]# @ S
+            # logging.info(f"New homography: {homographies[h]}")
     
-#     lir_filepath = 'lir.pkl'
-#     if os.path.exists(lir_filepath):
-#         with open(lir_filepath, 'rb') as f:
-#             largest_rect = pickle.load(f)
-#         x, y, w, h = largest_rect
-#         cropped_image = stitched_image_rgb[y:y+h, x:x+w]
-#     else:
-#         cropped_image = find_and_save_largest_inscribed_rectangle(mask, stitched_image_rgb, lir_filepath)
+    images_path = os.path.join(base_dir, 'final_images')
+    images_files = [f for f in os.listdir(images_path) if f.lower().endswith(('.jpg', '.png', '.bmp', '.jpeg'))]
+    images_indices = [int(os.path.splitext(f)[0]) for f in images_files]
+    # images = {i: cv2.resize(cv2.imread(os.path.join(images_path, f"{i}.jpg")), (0, 0), fx=resize_factor, fy=resize_factor) for i in images_indices}
+    images = {i: cv2.imread(os.path.join(images_path, f"{i}.jpg")) for i in images_indices}
 
-#     stitched_image_path = os.path.join(base_dir, 'stitched_image.jpg')
-#     cv2.imwrite(stitched_image_path, stitched_img)
+    # Step 1: Undistort Images
+    for i in images_indices:
+        K, D, mtx = calib_params[i]['K'], calib_params[i]['D'], calib_params[i]['mtx']
+        images[i] = cv2.undistort(images[i], K, D, None, mtx)
 
-#     cropped_image_path = os.path.join(base_dir, 'cropped_image.jpg')
-#     cv2.imwrite(cropped_image_path, cropped_image)
+    base_img = images[base_idx]
+    h, w = base_img.shape[:2]
 
-#     return stitched_image_rgb, cropped_image
+    corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype='float32').reshape(-1, 1, 2) # * resize_factor
+
+    all_corners = []
+    all_corners.extend(corners)
+    for i in images_indices:
+        if i == base_idx:
+            continue
+        if (base_idx, i) in homographies:
+            H = homographies[(base_idx, i)]
+            warped_corners = cv2.perspectiveTransform(corners, H)
+            # logging.info(f"Warped corners for image {i}: {warped_corners}")
+            all_corners.extend(warped_corners)
+
+    all_corners = np.array(all_corners)
+    [xmin, ymin] = np.int32(all_corners.min(axis=0).ravel())
+    [xmax, ymax] = np.int32(all_corners.max(axis=0).ravel())
+    t = [-xmin, -ymin]
+    H_translate = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]], dtype=np.float32)
+
+    output_width = xmax - xmin
+    output_height = ymax - ymin
+
+    warped_images = []
+    masks = []
+    for i in images_indices:
+        if i == base_idx:
+            continue
+        H = H_translate @ homographies[(base_idx, i)]
+        warped_image = cv2.warpPerspective(images[i], H, (output_width, output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+
+        # Create a mask based on the ROI
+        roi = calib_params[i]['roi']
+        x, y, w, h = roi
+        mask = np.zeros((images[i].shape[0], images[i].shape[1]), dtype=np.uint8)
+        mask[y:y+h, x:x+w] = 255
+
+        # Warp the mask using the same homography
+        warped_mask = cv2.warpPerspective(mask, H, (output_width, output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+
+        # Apply the mask to the warped image
+        masked_warped_image = cv2.bitwise_and(warped_image, warped_image, mask=warped_mask)
+
+        masks.append(warped_mask)
+        warped_images.append(masked_warped_image)
+
+    # Step 3: Blend images with feather blending
+    result = feather_blending(warped_images, masks, (output_height, output_width))
+    stitched_img = cv2.normalize(result, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    stitched_img = np.clip(stitched_img, 0, 255).astype(np.uint8)
+    stitched_image_rgb = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2RGB)
+
+    # Step 4: Find and crop the largest inscribed rectangle
+    mask = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+    
+    lir_filepath = 'lir.pkl'
+    if os.path.exists(lir_filepath):
+        logging.info("Loading largest inscribed rectangle.")
+        with open(lir_filepath, 'rb') as f:
+            largest_rect = pickle.load(f)
+        x, y, w, h = largest_rect
+        cropped_image = stitched_image_rgb[y:y+h, x:x+w]
+    else:
+        logging.info("Largest inscribed rectangle not found. Computing and saving.")
+        cropped_image = find_and_save_largest_inscribed_rectangle(mask, stitched_image_rgb, lir_filepath)
+
+    stitched_image_path = os.path.join(base_dir, 'stitched_image.jpg')
+    cv2.imwrite(stitched_image_path, stitched_img)
+
+    cropped_image_path = os.path.join(base_dir, 'cropped_image.jpg')
+    cv2.imwrite(cropped_image_path, cropped_image)
+
+    return stitched_image_rgb, cropped_image
 
 
 
@@ -535,6 +559,7 @@ if __name__ == "__main__":
     # Stage 1: Camera calibration
     parser.add_argument('-c', '--calibrate', action='store_true', help="Perform camera calibration.")
     parser.add_argument('-cr', '--calib_range', type=str, help="Comma-separated list of camera indices or ranges (e.g., '1-5,7-8,11-25').")
+    parser.add_argument('-rf', '--resize_factor', type=float, default=1.0, help="Resize factor for the images.")
     parser.add_argument('--fx', type=float, default=1328.89, help="Initial guess for focal length in x direction (f_x).")
     parser.add_argument('--fy', type=float, default=1493.33, help="Initial guess for focal length in y direction (f_y).")
     parser.add_argument('--cx', type=float, default=1280.0, help="Initial guess for principal point in x direction (c_x).")
@@ -562,14 +587,14 @@ if __name__ == "__main__":
 
     if args.calibrate:
         logging.info("Starting calibration.")
-        calib_params = calibrate_cameras(args.dir, args.calib_range, args.fx, args.fy, args.cx, args.cy)
+        calib_params = calibrate_cameras(args.dir, args.calib_range, args.fx, args.fy, args.cx, args.cy, args.resize_factor)
 
     if args.match:
         if not args.calibrate:
             logging.info("Loading calibration parameters.")
             calib_params = pickle.load(open(os.path.join(args.dir, "calib_params.pkl"), "rb"))
         logging.info("Matching chessboards between images.")
-        matched_points = match_images(args.dir, args.match_range, calib_params)
+        matched_points = match_images(args.dir, args.match_range, calib_params, args.resize_factor)
 
     if args.compute_homographies:
         if not args.calibrate:
@@ -589,6 +614,6 @@ if __name__ == "__main__":
             logging.info("Loading homographies.")
             homographies = pickle.load(open(os.path.join(args.dir, "homographies.pkl"), "rb"))
         logging.info("Stitching images.")
-        stitch_images(args.dir, calib_params, homographies, args.base_image)
+        stitch_images(args.dir, calib_params, homographies, args.base_image, args.resize_factor)
 
     logging.info("Script finished.")
