511ddc9afe79610f8ea73c8800c30ba9c2412cca
AlexLazarovich <alexlazarovch@gmail.com>
2024-12-24 16:19:46 +0200

    Delegating responsibilities

--- src/fov/src/ceiling_camera_node.py
+++ src/fov/src/ceiling_camera_node.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import Image
 from abstract_node import AbstractNode
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-
+from fov.srv import Coordinate, CoordinateRequest, CoordinateResponse
 
 class CeilingCameraNode(AbstractNode):
     def __init__(self):
@@ -21,11 +21,49 @@ class CeilingCameraNode(AbstractNode):
 
         # Set up the image publisher
         self.image_pub = rospy.Publisher(f'ceiling_camera_{self.camera_id}/image', Image, queue_size=10)
+        
         # OpenCV bridge
         self.bridge = CvBridge()
+        
+        # Load calibration parameters
+        self.base_idx = 3
+        self.resize_factor = 0.5
+        self.calib_params = pickle.load(open(os.path.join(param_dir, "calib_params.pkl"), "rb"))[self.camera_id]
+
+        # Load homography and calculate the scaling matrix
+        scaling_mtx = np.array([[self.resize_factor, 0, 0], [0, self.resize_factor, 0], [0, 0, 1]])
+        if self.camera_id == self.base_idx:
+            self.homography = scaling_mtx
+        else:            
+            homography = pickle.load(open(os.path.join(param_dir, "homographies.pkl"), "rb"))[(self.base_idx, i)]
+            self.homography = scaling_mtx @ homography
+
+        self.coordinate_set = rospy.ServiceProxy('image_stitcher/coordinates', Light)
 
         # Shutdown behavior
         rospy.on_shutdown(self.__on_shutdown)
+
+    def calc_params(self):
+        # Calculate the output image size and save coordinates for stitching node
+        _, img = self.camera.read()
+        h, w = img.shape[:2]
+        corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype='float32').reshape(-1, 1, 2)
+        corners = cv2.perspectiveTransform(corners, self.homography)
+        self.coordinates = {}
+        self.coordinates['xmin'], self.coordinates['ymin'] = np.int32(all_corners.min(axis=0).ravel())
+        self.coordinates['xmax'], self.coordinates['ymax'] = np.int32(all_corners.max(axis=0).ravel())
+        
+        self.output_width = self.coordinates['xmax'] - self.coordinates['xmin']
+        self.output_height = self.coordinates['ymax'] - self.coordinates['ymin']
+
+        x, y, w, h = self.calib_params[i]['roi']
+        mask = np.zeros(img.shape, dtype=np.uint8)
+        mask[y:y+h, x:x+w] = 255
+
+        self.mask = cv2.warpPerspective(mask, self.homography, (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+
+        rospy.wait_for_service('image_stitcher/coordinates')
+        self.coordinate_set(CoordinateRequest(idx=self.camera_id, xmin=self.coordinates['xmin'], ymin=self.coordinates['ymin'], xmax=self.coordinates['xmax'], ymax=self.coordinates['ymax']))
 
     def run(self):
         while not rospy.is_shutdown():
@@ -33,7 +71,10 @@ class CeilingCameraNode(AbstractNode):
                 ret, img = self.camera.read()
                 if ret:
                     # Publish the image
-                    img_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
+                    img = cv2.undistort(img, self.calib_params['K'], self.calib_params['D'], None, self.calib_params['mtx'])
+                    warped_image = cv2.warpPerspective(img, self.homography, (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+                    masked_warped_image = cv2.bitwise_and(warped_image, warped_image, mask=self.mask)
+                    img_msg = self.bridge.cv2_to_imgmsg(warped_image, "bgr8")
                     self.image_pub.publish(img_msg)
                 else:
                     rospy.logwarn(f"Camera {self.camera_id}: No image captured.")
@@ -41,6 +82,9 @@ class CeilingCameraNode(AbstractNode):
                 rospy.logerr(f"Camera {self.camera_id}: CV Bridge Error - {e}")
 
             rospy.sleep(0.05)
+
+    def get_coordinates(self):
+        return self.coordinates
 
     def __on_shutdown(self):
         rospy.loginfo(f"Ceiling Camera {self.camera_id}: Releasing camera.")
--- src/fov/src/image_stitching_node.py
+++ src/fov/src/image_stitching_node.py
@@ -3,6 +3,7 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from fov.srv import Coordinate, CoordinateRequest, CoordinateResponse
 import cv2
 import pickle
 import os
@@ -25,6 +26,7 @@ class ImageStitchingNode:
         for i in range(1, 6):
             rospy.Subscriber(self.sub_topic_format.format(i), Image, self.image_callback, callback_args=i)
 
+        # Load calibration parameters
         param_dir = r'/home/icvl/fov_ws/src/fov/src/etc'
 
         self.calib_params = pickle.load(open(os.path.join(param_dir, "calib_params.pkl"), "rb"))
@@ -33,11 +35,18 @@ class ImageStitchingNode:
 
         self.base_idx = 3
         self.parameters_set = False
+        self.coordinates = {}
  
         # Publisher for the stitched image
         self.stitched_image_pub = rospy.Publisher("image_stitcher/image", Image, queue_size=10)
+        rospy.Service('image_stitcher/coordinates', Coordinate, self.set_coordinates)
 
         rospy.on_shutdown(self.__on_shutdown)
+
+    def set_coordinates(self, data: CoordinateRequest):
+        self.coordinates[data.idx] = {'x.min': data.xmin, 'y.min': data.ymin, 'x.max': data.xmax, 'y.max': data.ymax}
+        if len(self.coordinates) == 5:
+            self.calculate_parameters()
 
     def image_callback(self, msg, index):
         try:
@@ -71,20 +80,23 @@ class ImageStitchingNode:
             rate.sleep()
 
     def calculate_parameters(self):
-        h, w = self.images[self.base_idx].shape[:2]
+        # h, w = self.images[self.base_idx].shape[:2]
 
-        corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype='float32').reshape(-1, 1, 2)
+        # corners = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype='float32').reshape(-1, 1, 2)
+        # all_corners = []
+        # all_corners.extend(corners)
+        # for i in range(1,6):
+        #     if i == self.base_idx:
+        #         continue
+        #     if (self.base_idx, i) in self.homographies:
+        #         H = self.homographies[(self.base_idx, i)]
+        #         warped_corners = cv2.perspectiveTransform(corners, H)
+        #         all_corners.extend(warped_corners)
         all_corners = []
-        all_corners.extend(corners)
         for i in range(1,6):
-            if i == self.base_idx:
-                continue
-            if (self.base_idx, i) in self.homographies:
-                H = self.homographies[(self.base_idx, i)]
-                warped_corners = cv2.perspectiveTransform(corners, H)
-                all_corners.extend(warped_corners)
+            all_corners.extend([self.coordinates[i]['x.min'], self.coordinates[i]['y.min'], self.coordinates[i]['x.max'], self.coordinates[i]['y.max']])
+        all_corners = np.array(all_corners).reshape(-1, 2)
 
-        all_corners = np.array(all_corners)
         [xmin, ymin] = np.int32(all_corners.min(axis=0).ravel())
         [xmax, ymax] = np.int32(all_corners.max(axis=0).ravel())
         t = [-xmin, -ymin]
@@ -93,26 +105,26 @@ class ImageStitchingNode:
         self.output_width = xmax - xmin
         self.output_height = ymax - ymin
 
-        self.masks = []
-        self.final_homographies={}
-        for i in range(1,6):
-            if i == self.base_idx:
-                continue
-            self.final_homographies[i] = self.H_translate @ self.homographies[(self.base_idx, i)]
+        # self.masks = []
+        # self.final_homographies={}
+        # for i in range(1,6):
+        #     if i == self.base_idx:
+        #         continue
+            # self.final_homographies[i] = self.H_translate @ self.homographies[(self.base_idx, i)]
             # warped_image = cv2.warpPerspective(self.images[i], H, (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
 
             # Create a mask based on the ROI
-            roi = self.calib_params[i]['roi']
-            x, y, w, h = roi
-            mask = np.zeros((self.images[i].shape[0], self.images[i].shape[1]), dtype=np.uint8)
-            mask[y:y+h, x:x+w] = 255
+            # roi = self.calib_params[i]['roi']
+            # x, y, w, h = roi
+            # mask = np.zeros((self.images[i].shape[0], self.images[i].shape[1]), dtype=np.uint8)
+            # mask[y:y+h, x:x+w] = 255
 
             # Warp the mask using the same homography
-            warped_mask = cv2.warpPerspective(mask, self.final_homographies[i], (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+            # warped_mask = cv2.warpPerspective(mask, self.final_homographies[i], (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
 
             # Apply the mask to the warped image
 
-            self.masks.append(warped_mask)
+            # self.masks.append(warped_mask)
 
         self.parameters_set = True
 
@@ -127,39 +139,44 @@ class ImageStitchingNode:
 
     def stitch_images(self, num_bands=5, sigma=1):
         # Step 1: Undistort Images
-        for i in range(1,6):
-            K, D, mtx = self.calib_params[i]['K'], self.calib_params[i]['D'], self.calib_params[i]['mtx']
-            self.images[i] = cv2.undistort(self.images[i], K, D, None, mtx)
+        # for i in range(1,6):
+        #     K, D, mtx = self.calib_params[i]['K'], self.calib_params[i]['D'], self.calib_params[i]['mtx']
+        #     self.images[i] = cv2.undistort(self.images[i], K, D, None, mtx)
 
-        if not self.parameters_set:
-            self.calculate_parameters()
+        # if not self.parameters_set:
+        #     self.calculate_parameters()
 
-        warped_images = []
-        # masks = []
-        for i in range(1,6):
-            if i == self.base_idx:
-                continue
-            # H = self.H_translate @ self.homographies[(self.base_idx, i)]
-            warped_image = cv2.warpPerspective(self.images[i], self.final_homographies[i], (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+        # warped_images = []
+        # # masks = []
+        # for i in range(1,6):
+        #     if i == self.base_idx:
+        #         continue
+        #     # H = self.H_translate @ self.homographies[(self.base_idx, i)]
+        #     # warped_image = cv2.warpPerspective(self.images[i], self.final_homographies[i], (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
 
-            # Create a mask based on the ROI
-            roi = self.calib_params[i]['roi']
-            x, y, w, h = roi
-            # mask = np.zeros((self.images[i].shape[0], self.images[i].shape[1]), dtype=np.uint8)
-            # mask[y:y+h, x:x+w] = 255
+        #     # Create a mask based on the ROI
+        #     roi = self.calib_params[i]['roi']
+        #     x, y, w, h = roi
+        #     # mask = np.zeros((self.images[i].shape[0], self.images[i].shape[1]), dtype=np.uint8)
+        #     # mask[y:y+h, x:x+w] = 255
 
-            # Warp the mask using the same homography
-            # warped_mask = cv2.warpPerspective(mask, H, (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+        #     # Warp the mask using the same homography
+        #     # warped_mask = cv2.warpPerspective(mask, H, (self.output_width, self.output_height), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
 
-            # Apply the mask to the warped image
-            masked_warped_image = cv2.bitwise_and(warped_image, warped_image, mask=self.masks[i-1] if i<self.base_idx else self.masks[i-2])
+        #     # Apply the mask to the warped image
+        #     # masked_warped_image = cv2.bitwise_and(warped_image, warped_image, mask=self.masks[i-1] if i<self.base_idx else self.masks[i-2])
 
-            # masks.append(warped_mask)
-            warped_images.append(masked_warped_image)
+        #     # masks.append(warped_mask)
+        #     warped_images.append(masked_warped_image)
 
         # Step 3: Blend images with feather blending
+        translated_images = []
+        for i in range(1,6):
+            translated_image = self.H_translate @ self.images[i]
+            translated_images.append(translated_image)
+
         t0=time.time()
-        result = self.feather_blending(warped_images, self.masks, (self.output_height, self.output_width))
+        result = self.feather_blending(translated_images, self.masks, (self.output_height, self.output_width))
         rospy.loginfo(f"Blending: {time.time()-t0}")
         stitched_img = cv2.normalize(result, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
         stitched_img = np.clip(stitched_img, 0, 255).astype(np.uint8)
